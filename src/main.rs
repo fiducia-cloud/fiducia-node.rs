79a7804dbc7682d9a200d8a81baeedbb44cb1998
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 17:10:37 -0500

    claude autosave 2026-07-12T22:10:37Z

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let v1 = Router::new()
         .route("/status", get(status))
         .nest("/kv", kv::router())
+        .nest("/counters", counters::router())
         .nest("/idempotency", idempotency::router())
         .nest("/locks", locks::router())
         .nest("/semaphores", semaphore::router())
