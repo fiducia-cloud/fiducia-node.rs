6119ab76397a0804f25a0ffaa3dfb9253b5f71e4
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-17 12:09:18 -0500

    claude autosave 2026-07-17T17:09:18Z

--- src/observe.rs
+++ src/observe.rs
@@ -302,6 +302,7 @@ mod tests {
                 locks(
                     State(node.clone()),
                     org.clone(),
+                    admin_headers(),
                     Uri::from_static("/v1/observe/locks"),
                 )
                 .await,
